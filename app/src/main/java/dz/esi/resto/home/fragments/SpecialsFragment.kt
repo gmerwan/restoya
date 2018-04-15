a2c1c6cf9c006fc18588faf0986418f2488238e0
gmerwan <cm_ghomrani@esi.dz>
2018-04-14 18:08:11 -0700

    Initial commit + basic stuff

---
+++ app/src/main/java/dz/esi/resto/home/fragments/SpecialsFragment.kt
@@ -0,0 +1,25 @@
+package dz.esi.resto.home.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import dz.esi.resto.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SpecialsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specials, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/dz/esi/resto/start/SplachScreenActivity.kt
@@ -0,0 +1,20 @@
+package dz.esi.resto.start
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import dz.esi.resto.home.activities.HomeActivity
+import dz.esi.resto.R
+import org.jetbrains.anko.startActivity
+
+class SplachScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splach_screen)
+
+        Handler().postDelayed({
+            startActivity<HomeActivity>()
+        }, 5000)
+    }
+}
